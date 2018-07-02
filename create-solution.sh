bb4c0485658113dfd78d725f5dabc3fc6b8801bd
Steven Colburn <steven@>
2018-07-02 16:44:19 -0700

    command to creat folder structure for dotnet

---
+++ create-solution.sh
@@ -0,0 +1,45 @@
+# To Run a Command go to location and type ./(command)
+# Example:
+# cd Commands
+# ./create-solution.sh
+
+
+cd ..
+mkdir "$1.Solution"
+cd ./$1.Solution/
+
+touch clear.sh
+touch commit.sh
+touch fresh.sh
+touch run.sh
+
+echo "rm -rf ./$1/bin/
+rm -rf ./$1/obj/
+./fresh.sh" > clear.sh
+
+echo "git add .
+git-pair-commit -m \"\$1\"" > commit.sh
+
+echo "dotnet restore ./$1
+dotnet build ./$1" > fresh.sh
+
+echo "cd $1/
+dotnet run" > run.sh
+
+chmod 755 ./clear.sh
+chmod 755 ./commit.sh
+chmod 755 ./fresh.sh
+chmod 755 ./run.sh
+
+touch README.md
+dotnet new web -lang "C#" -f "netcoreapp1.1" -n "$1"
+cd ./$1/
+dotnet add "$1.csproj" package Microsoft.AspNetCore.Http -v 1.1.2
+dotnet add "$1.csproj" package Microsoft.AspNetCore.Mvc -v 1.1.3
+dotnet add "$1.csproj" package Microsoft.AspNetCore.StaticFiles -v 1.1.3
+dotnet restore
+mkdir Controllers Models Views ./wwwroot/css ./wwwroot/js
+mkdir ./Views/Home
+touch ./Views/Home/template.cshtml
+touch ./Controllers/HomeController.cs
+touch ./Models/template.cs
