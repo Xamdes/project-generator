10c8b4af56c80ca097864368d065c3345ce8ad43
Steven Colburn <stevencolburn@xamdes.com>
2018-08-20 16:32:34 -0700

    update create angular shell script

--- create-angular-project.sh
+++ create-angular-project.sh
@@ -3,3 +3,12 @@ ng new $1
 cd ./$1
 npm install ajv hoek bootstrap --save
 npm audit fix --force
+
+mkdir commands
+cd commands
+touch component.sh
+echo "#!/usr/bin/env bash
+ng g component \$1
+" > ./component.sh
+
+chmod 755 *
