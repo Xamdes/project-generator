3f1268b0e5668cfb1355732c6e80fb81aa607b95
Steven Colburn <stevencolburn@xamdes.com>
2018-09-06 10:33:43 -0700

    update react to create default readme file

--- bin/react-project.sh
+++ bin/react-project.sh
@@ -382,3 +382,44 @@ const Main = styled.div\`
 
 echo "*.md
 " > .eslintignore
+
+echo "# React Help Queue
+
+## Author(s)
+
+  * Jane Doe
+
+## Install
+
+\`\`\`
+  git clone https://github.com/[github-username]/$1.git
+  cd $1
+  npm install
+\`\`\`
+
+## Instructions
+
+* npm run fresh
+  * installs packages from package.json and run webpack
+* npm run gen [component-name]
+  * generates a component bast on given component name
+  * puts component in ./src/components/ directory
+* npm run commit '[message]'
+  * runs git commit -m using npm
+* npm run git-cinfig '[name]' '[email]'
+  * configures local git username and email
+* npm run start
+  * starts webpack-dev-server
+  * http://localhost:8080
+* npm run start-web
+  * starts webpack-dev-server and opens http://localhost:8080 in browser (Chrome)
+* npm run web
+  * opens http://localhost:8080
+* npm run lint
+  * runs eslint on files in ./src/
+* npm run lint-fix
+  * runs eslint and tells it to attempt to fix errors in files located in ./src/
+
+## Specifications
+
+" > README.md
